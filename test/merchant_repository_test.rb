49db0bf323bc100c947fb68030056907945dcc25
Michael Gatewood <michael@mngatewood.com>
2018-09-09 00:03:07 -0600

    Abbreviates @merchant_repository in MerchantRepositoryTest

--- test/merchant_repository_test.rb
+++ test/merchant_repository_test.rb
@@ -7,7 +7,7 @@ require_relative '../lib/merchant'
 class MerchantRepositoryTest < Minitest::Test
 
   def setup
-    @merchant_repository = MerchantRepository.new
+    @mr = MerchantRepository.new
     @merchant_1 = Merchant.new({:id => 1, :name => "Shopin1901"})
     @merchant_2 = Merchant.new({:id => 2, :name => "Candisart"})
     @merchant_3 = Merchant.new({:id => 3, :name => "Not Candy Crack"})
@@ -16,35 +16,35 @@ class MerchantRepositoryTest < Minitest::Test
   end
 
   def add_test_merchants
-    @merchant_repository.add_merchant(@merchant_1)
-    @merchant_repository.add_merchant(@merchant_2)
-    @merchant_repository.add_merchant(@merchant_3)
-    @merchant_repository.add_merchant(@merchant_4)
-    @merchant_repository.add_merchant(@merchant_5)
+    @mr.add_merchant(@merchant_1)
+    @mr.add_merchant(@merchant_2)
+    @mr.add_merchant(@merchant_3)
+    @mr.add_merchant(@merchant_4)
+    @mr.add_merchant(@merchant_5)
   end
 
   def test_it_exist
-    assert_instance_of MerchantRepository, @merchant_repository
+    assert_instance_of MerchantRepository, @mr
   end
 
   def test_it_starts_with_no_merchants
-    assert_equal [], @merchant_repository.merchants
+    assert_equal [], @mr.merchants
   end
 
   def test_can_add_a_merchant
     m = Merchant.new({:id => 5, :name => "Turing School"})
-    @merchant_repository.add_merchant(m)
-    assert_equal [m], @merchant_repository.merchants
+    @mr.add_merchant(m)
+    assert_equal [m], @mr.merchants
   end
 
   def test_can_add_several_merchants
-    @merchant_repository.add_merchant(@merchant_1)
-    @merchant_repository.add_merchant(@merchant_2)
-    @merchant_repository.add_merchant(@merchant_3)
-    @merchant_repository.add_merchant(@merchant_4)
-    @merchant_repository.add_merchant(@merchant_5)
+    @mr.add_merchant(@merchant_1)
+    @mr.add_merchant(@merchant_2)
+    @mr.add_merchant(@merchant_3)
+    @mr.add_merchant(@merchant_4)
+    @mr.add_merchant(@merchant_5)
     expected = [@merchant_1, @merchant_2, @merchant_3, @merchant_4, @merchant_5]
-    assert_equal expected, @merchant_repository.merchants
+    assert_equal expected, @mr.merchants
   end
 
   def test_it_can_return_all_merchants
