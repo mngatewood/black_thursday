7d5cc2de707113fd57d4128b71d9f43daa5d981c
Michael Gatewood <michael@mngatewood.com>
2018-09-09 00:00:04 -0600

    Updates Item to include ID

--- lib/item.rb
+++ lib/item.rb
@@ -1,15 +1,19 @@
 class Item
-  attr_reader :name,
+
+  attr_reader :id,
+              :name,
               :description,
               :unit_price,
               :created_at,
               :updated_at
 
   def initialize(info)
-    @name = info[:name]
-    @description = info[:description]
-    @unit_price = info[:unit_price]
-    @created_at = info[:created_at]
-    @updated_at = info[:updated_at]
+    @id           = info[:id]
+    @name         = info[:name]
+    @description  = info[:description]
+    @unit_price   = info[:unit_price]
+    @created_at   = info[:created_at]
+    @updated_at   = info[:updated_at]
   end
+  
 end
