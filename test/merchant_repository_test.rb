f1cad38dd80049fbf57c1fd2f347dc821c5d4a8b
Michael Gatewood <michael@mngatewood.com>
2018-09-09 00:30:54 -0600

    Abbreviates missed references to merchant_repository in tests

--- test/merchant_repository_test.rb
+++ test/merchant_repository_test.rb
@@ -50,49 +50,49 @@ class MerchantRepositoryTest < Minitest::Test
   def test_it_can_return_all_merchants
     self.add_test_merchants
     expected = [@merchant_1, @merchant_2, @merchant_3, @merchant_4, @merchant_5]
-    assert_equal expected, @merchant_repository.all
+    assert_equal expected, @mr.all
   end
 
   def test_it_can_find_a_merchant_by_id
     self.add_test_merchants
-    assert_equal @merchant_repository.find_by_id(1), @merchant_1
-    assert_nil @merchant_repository.find_by_id(0)
+    assert_equal @mr.find_by_id(1), @merchant_1
+    assert_nil @mr.find_by_id(0)
 
   end
   
   def test_it_can_find_a_single_merchant_by_name
     self.add_test_merchants
-    assert_equal @merchant_2, @merchant_repository.find_by_name("Candisart")
-    assert_nil @merchant_repository.find_by_name("Not A Real Merchant")
+    assert_equal @merchant_2, @mr.find_by_name("Candisart")
+    assert_nil @mr.find_by_name("Not A Real Merchant")
   end
   
   def test_it_can_find_all_merchants_by_partial_name
     self.add_test_merchants
     expected = [@merchant_2, @merchant_3]
-    assert_equal expected, @merchant_repository.find_all_by_name("cand")
-    assert_equal [], @merchant_repository.find_all_by_name("notgonnafindit")
+    assert_equal expected, @mr.find_all_by_name("cand")
+    assert_equal [], @mr.find_all_by_name("notgonnafindit")
   end
 
   def test_it_can_create_a_new_merchant
     self.add_test_merchants
     attributes = {:name => "Nerds Inc."}
-    @merchant_repository.create(attributes)
-    assert_equal 6, @merchant_repository.merchants.last.id
-    assert_equal "Nerds Inc.", @merchant_repository.merchants.last.name
+    @mr.create(attributes)
+    assert_equal 6, @mr.merchants.last.id
+    assert_equal "Nerds Inc.", @mr.merchants.last.name
   end
 
   def test_it_can_update_merchant_attributes
     self.add_test_merchants
     attributes = {:name => "Candy For Adults"}
-    @merchant_repository.update(3, attributes)
-    assert_equal "Candy For Adults", @merchant_repository.find_by_id(3).name
+    @mr.update(3, attributes)
+    assert_equal "Candy For Adults", @mr.find_by_id(3).name
   end
 
   def test_it_can_delete_a_merchant_by_id
     self.add_test_merchants
-    @merchant_repository.delete(1)
-    assert_nil @merchant_repository.find_by_id(1)
-    assert_nil @merchant_repository.find_by_name("Shopin1901")
+    @mr.delete(1)
+    assert_nil @mr.find_by_id(1)
+    assert_nil @mr.find_by_name("Shopin1901")
   end
   
 end
