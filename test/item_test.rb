6aa7ddd0d7103292a1a5b3ea4745c0057f25900c
Michael Gatewood <michael@mngatewood.com>
2018-09-09 00:00:38 -0600

    Adds id and setup to ItemTest

--- test/item_test.rb
+++ test/item_test.rb
@@ -1,33 +1,33 @@
 require 'minitest/autorun'
 require 'minitest/pride'
 require 'bigdecimal'
+require 'time'
 require_relative '../lib/item'
 
 class ItemTest < Minitest::Test
+
+  def setup
+    @item = Item.new({:id => 1, 
+                      :name => "Pencil",
+                      :description => "You can use it to write things",
+                      :unit_price => BigDecimal.new(10.99,4),
+                      :created_at => Time.new(2018, 9, 8, 0, 0, 0, "-06:00"),
+                      :updated_at => Time.new(2018, 9, 9, 0, 0, 0, "-06:00")
+                    })
+  end
+
   def test_it_exist
-    i = Item.new({
-      :name        => "Pencil",
-      :description => "You can use it to write things",
-      :unit_price  => BigDecimal.new(10.99,4),
-      :created_at  => Time.now,
-      :updated_at  => Time.now,
-    })
-    assert_instance_of Item, i
+    assert_instance_of Item, @item
   end
 
   def test_it_has_attributes
-    i = Item.new({
-      :name        => "Pencil",
-      :description => "You can use it to write things",
-      :unit_price  => BigDecimal.new(10.99,4),
-      :created_at  => Time.new(2018, 9, 8),
-      :updated_at  => Time.new(2018, 10, 8),
-    })
 
-    assert_equal "Pencil", i.name
-    assert_equal "You can use it to write things", i.description
-    assert_equal BigDecimal.new(10.99,4), i.unit_price
-    assert_equal "2018-09-08 00:00:00 -0600", i.created_at.to_s
-    assert_equal "2018-10-08 00:00:00 -0600", i.updated_at.to_s
+    assert_equal 1, @item.id
+    assert_equal "Pencil", @item.name
+    assert_equal "You can use it to write things", @item.description
+    assert_equal BigDecimal.new(10.99,4), @item.unit_price
+    assert_equal "2018-09-08 00:00:00 -0600", @item.created_at.to_s
+    assert_equal "2018-09-09 00:00:00 -0600", @item.updated_at.to_s
   end
+
 end
