cdbdfce757e3705cf3ac63f5264e0c75138198e5
Michael Gatewood <michael@mngatewood.com>
2018-09-13 14:43:53 -0600

    Converts transaction.result to symbol in SalesEngine

--- lib/sales_engine.rb
+++ lib/sales_engine.rb
@@ -110,7 +110,7 @@ class SalesEngine
       :invoice_id                   => data[:invoice_id],
       :credit_card_number           => data[:credit_card_number],
       :credit_card_expiration_date  => data[:credit_card_expiration_date],
-      :result                       => data[:result],
+      :result                       => data[:result].to_sym,
       :created_at                   => Time.parse(data[:created_at]),
       :updated_at                   => Time.parse(data[:updated_at])
       })
